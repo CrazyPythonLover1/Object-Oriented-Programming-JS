b653e37074e7130691e421a7bda97a469e4f79e4
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-10-19 23:39:04 +0600

    abstraction

---
+++ abstraction.js
@@ -0,0 +1,30 @@
+class Animal{
+    constructor(){
+
+    }
+    talk(){
+        // console.log("make some noise")
+        throw new Error("you must implement talk method")
+    }
+
+}
+
+class Cat extends Animal{
+    talk(){
+        console.log("meow meow")
+    }
+}
+
+class Dog extends Animal{
+    talks(){
+        console.log("bark bark")
+    }
+    talk(){
+        console.log("bark bark")
+    }
+}
+
+const myCat = new Cat();
+myCat.talk();
+const myDog = new Dog();
+myDog.talk();
